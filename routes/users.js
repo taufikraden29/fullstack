579d3a30dd7caaa2e8fb53ab4a3526580d7aeb85
@taufikraden29 <taufikraden29@gmail.com>
2022-11-08 20:19:24 +0700

    update

--- routes/users.js
+++ routes/users.js
@@ -6,9 +6,46 @@ const UsersModel = require('../models/users')
 router.get('/', async(req, res) => {
     const users = await UsersModel.findAll()
     res.status(200).json({
-        data : "suka makan",
+        data : users,
         name : "Ramuirta"
     })
+})
+
+router.post('/', async(req, res) => {
+    // REQEUST DATA
+    const { nip, nama, password } = req.body
+
+    const users = await UsersModel.create({
+        nip, nama, password
+    })
+    res.status(200).json({
+        data : users,
+        name : "add Post Man"
+    })
+})
+
+router.put('/', async(req, res) => {
+    // REQEUST DATA
+    const { nip, nama, password, passwordBaru } = req.body
+
+    // CEK ID
+    const userData = await UsersModel.findOne({ where: { nip: nip } }) 
+    console.log(userData)
+
+    // PASSWORD YG MNCUL DAI DB === password dari inputan
+    if(userData.password === password) {
+        const users = await UsersModel.update({
+            nama, password: passwordBaru
+        }, { where: { nip: nip } })
+        res.status(200).json({
+            users: { updated: users[0] },
+            metadata: "Users updated!"
+        })
+    } else {
+        res.status(400).json({
+            error: "data invalid, masukan kembali!"
+        })
+    }
 })
 
 module.exports = router
